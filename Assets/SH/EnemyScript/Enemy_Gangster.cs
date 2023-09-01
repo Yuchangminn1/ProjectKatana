28b75c117b15e31e37dde018a2cd954127a59b32
Raidanin <116862789+Raidanin@users.noreply.github.com>
2023-09-01 16:03:44 +0900

    9/01 work
    gangster 구현

--- Assets/SH/EnemyScript/Enemy_Gangster.cs
+++ Assets/SH/EnemyScript/Enemy_Gangster.cs
@@ -3,22 +3,14 @@ using UnityEngine;
 public class Enemy_Gangster : Enemy_SH
 {
     float pDistance;
-    [SerializeField] GameObject daggerPrefab;
-    [SerializeField] int daggerCount = 1;
-    [SerializeField] Transform daggerFirepos;
-
-    [SerializeField] private float throwCooldown;
-    [SerializeField] private float p1Cooldown;
-    [SerializeField] private float dodgeCooldown;
+    [SerializeField] GameObject BulletPrefap;
+    [SerializeField] Transform Firepos;
 
     private Animator animator;
 
-    private float throwCooldownTimer;
-    private float p1CooldownTimer;
-    private float dodgeCooldownTimer;
-    private float attackCooldownTimer;
+    [SerializeField] private float shootCooldown;
+    private float shootCooldownTimer;
 
-    [SerializeField] private float dashDistance = 3;
     [SerializeField] private float throwDistance = 7;
 
     bool isBusy = false;
@@ -43,7 +35,7 @@ public class Enemy_Gangster : Enemy_SH
 
         idleState = new GangsterIdleState(this, stateMachine, "Idle", this);
         shootState = new GangsterShootState(this, stateMachine, "Shoot", this);
-        moveState = new GangsterMoveState(this, stateMachine, "Dash", this);
+        moveState = new GangsterMoveState(this, stateMachine, "Move", this);
         //hitState = new GangsterHitState(this, stateMachine, "Hit", this);
 
 
@@ -54,9 +46,6 @@ public class Enemy_Gangster : Enemy_SH
         base.Start();
 
         stateMachine.Initialize(idleState);
-        throwCooldownTimer = throwCooldown; //쿨다운이 다 되면 언제든지 공격 할 수 있도록 하려면?
-        p1CooldownTimer = p1Cooldown;
-        dodgeCooldownTimer = dodgeCooldown;
 
     }
 
@@ -67,24 +56,16 @@ public class Enemy_Gangster : Enemy_SH
         {
             Destroy(gameObject);
         }
-        if (throwCooldownTimer > 0)
-            throwCooldownTimer -= Time.deltaTime;
-        if (p1CooldownTimer > 0)
-            p1CooldownTimer -= Time.deltaTime;
-        if (dodgeCooldownTimer > 0)
-            dodgeCooldownTimer -= Time.deltaTime;
-        if (attackCooldownTimer > 0)
+
+
+        if (shootCooldownTimer > 0)
         {
-            attackCooldownTimer -= Time.deltaTime;
+            shootCooldownTimer -= Time.deltaTime;
         }
 
         if (player != null)
         {
             pDistance = Vector2.Distance(transform.position, player.transform.position);
-
-
-            if (pDistance < attackDistance && !isBusy && attackCooldownTimer <= 0)
-                stateMachine.ChangeState(shootState);
 
         }
 
@@ -125,9 +106,8 @@ public class Enemy_Gangster : Enemy_SH
         public override void Update()
         {
             base.Update();
-            gangster.FacingPlayer();
             if (stateTimer < 0 && !gangster.isBusy)
-                stateMachine.ChangeState(gangster.shootState);
+                stateMachine.ChangeState(gangster.moveState);
         }
     }
 
@@ -144,11 +124,14 @@ public class Enemy_Gangster : Enemy_SH
         public override void Enter()
         {
             base.Enter();
+
+            stateTimer = gangster.idleTime;
         }
 
         public override void Exit()
         {
             base.Exit();
+            gangster.shootCooldownTimer = gangster.shootCooldown;
         }
 
         public override void Update()
@@ -156,14 +139,9 @@ public class Enemy_Gangster : Enemy_SH
 
             base.Update();
             gangster.FacingPlayer();
-            gangster.SetVelocity(gangster.moveSpeed * gangster.FacingDir * 2, rb.velocity.y);
+            gangster.SetVelocity(gangster.moveSpeed * gangster.FacingDir , rb.velocity.y);
 
-            if (gangster.pDistance < gangster.dashDistance)
-            {
-                stateMachine.ChangeState(gangster.moveState);
-            }
-
-            if (gangster.pDistance > gangster.throwDistance && gangster.throwCooldownTimer <= 0)
+            if(gangster.shootCooldownTimer <= 0 && stateTimer <= 0)
             {
                 stateMachine.ChangeState(gangster.shootState);
             }
@@ -183,6 +161,7 @@ public class Enemy_Gangster : Enemy_SH
         {
             base.Enter();
             gangster.SetVelocityToZero();
+            gangster.Shoot();
         }
 
         public override void Exit()
@@ -195,7 +174,7 @@ public class Enemy_Gangster : Enemy_SH
         {
             base.Update();
 
-            if (gangster.throwCooldownTimer > 0)
+            if (gangster.shootCooldownTimer > 0)
             {
                 stateMachine.ChangeState(gangster.idleState);
             }
@@ -223,7 +202,7 @@ public class Enemy_Gangster : Enemy_SH
         {
             base.Exit();
             gangster.hp--;
-            gangster.daggerCount++;
+
         }
 
         public override void Update()
@@ -234,18 +213,13 @@ public class Enemy_Gangster : Enemy_SH
 
 
 
-    public void ThrowDagger()
+    public void Shoot()
     {
 
-        for (int i = 0; i < daggerCount; i++)
-        {
-            // 단검이 퍼져 나가는 각도를 계산
 
-            // 단검을 생성하고 회전
-            Instantiate(daggerPrefab, daggerFirepos.position, Quaternion.identity);
-        }
+        Instantiate(BulletPrefap, Firepos.position, Quaternion.identity , transform);
 
-        throwCooldownTimer = throwCooldown;
+
 
     }
 
