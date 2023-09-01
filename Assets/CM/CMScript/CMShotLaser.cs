2f26bc721525036438504e337353ba239d5309df
ycm23 <68059564+Yuchangminn1@users.noreply.github.com>
2023-09-01 10:07:07 +0900

    laserShot

--- Assets/CM/CMScript/CMShotLaser.cs
+++ Assets/CM/CMScript/CMShotLaser.cs
@@ -8,13 +8,16 @@ public class CMShotLaser : MonoBehaviour
     [SerializeField] GameObject cmLaser;
     [SerializeField] GameObject[] cmLaserList;
     [SerializeField] int laserNum = 15;
+    [SerializeField] bool isShoot = true;
     [SerializeField] float laserMoveSpeed = 1;
     [SerializeField] Vector3 laserScale;
     [SerializeField] float yReturn = -1.360f;
 
+
     private void Start()
     {
-        if (laserScale == null || laserScale == Vector3.zero ){
+        if (laserScale == null || laserScale == Vector3.zero)
+        {
             laserScale = Vector3.one;
         }
         int i = 0;
@@ -22,37 +25,50 @@ public class CMShotLaser : MonoBehaviour
         while (i < laserNum)
         {
             cmLaserList[i] = Instantiate(cmLaser);
-
             cmLaserList[i].transform.parent = transform;
             cmLaserList[i].transform.localScale = laserScale;
-            StartCoroutine(ShotNReturn(cmLaserList[i], 0.2f * i ));
-            
+            cmLaserList[i].transform.position = new Vector2(cmLaserList[i].transform.position.x, cmLaserList[i].transform.position.y + 3f);
             ++i;
-
         }
+
+        StartCoroutine(ShotE());
     }
 
-    IEnumerator ShotNReturn(GameObject _gameObject, float _startTime)
+    IEnumerator ShotNReturn(GameObject _gameObject, float _startTime,int _i)
     {
         float yPos = _gameObject.transform.position.y;
-        yield return new WaitForSeconds(_startTime);
+        
+        if (_i == laserNum - 1)
+        {
+            StartCoroutine(ShotE());
+        }
         while (true)
         {
-            //Debug.Log(_startTime);
             if (yPos < yReturn)
             {
                 yPos = shotPos.position.y;
-                Debug.Log("yRewturn");
+                _gameObject.transform.position = new Vector2(shotPos.position.x, yPos);
+                break;
             }
             else yPos = _gameObject.transform.position.y - Time.deltaTime * laserMoveSpeed;
             _gameObject.transform.position = new Vector2(shotPos.position.x, yPos);
 
-            yield return null;
+            yield return new WaitForFixedUpdate();
 
-            
+        }
+    }
+    IEnumerator ShotE()
+    {
+        int i = 0;
+      
+        while (i < laserNum)
+        {
+            StartCoroutine(ShotNReturn(cmLaserList[i], 0.2f * i, i));
+            ++i;
+            yield return new WaitForFixedUpdate();
+            yield return new WaitForFixedUpdate();
+
         }
 
-        yield return null;
     }
-
 }
