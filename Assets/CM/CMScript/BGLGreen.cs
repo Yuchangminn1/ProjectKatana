cbad4b3024fe086dd0cec005402628b4d73039b2
ycm23 <68059564+Yuchangminn1@users.noreply.github.com>
2023-09-04 17:48:33 +0900

    Stage4

--- Assets/CM/CMScript/BGLGreen.cs
+++ Assets/CM/CMScript/BGLGreen.cs
@@ -6,24 +6,26 @@ using UnityEngine.Rendering.Universal;
 
 public class BGLGreen : LightBlink
 {
+    
     [Header("BGLGreen")]
 
     [SerializeField] protected int coroutineNum =0;
 
     [SerializeField] protected float StartTime = 0;
 
+    [Header("Move 0  circlestart ")]
 
-    [Header("Move 1 ")]
+    [Header("Move 1  Updown ")]
     [SerializeField] protected float originX;
     [SerializeField] protected float originY;
 
     [SerializeField] protected float startAngle = 1f / 4f;
     [SerializeField] protected float currentAngle = 1f/4f;
-    [SerializeField] protected float divAngle = 100;
+    [SerializeField] protected float divAngle = 45;
 
     [SerializeField] protected float circleR = 3f;
-    [Header("Move 2 ")]
-    [SerializeField] protected float maxMove;
+    [Header("Move 2 circle")]
+    [SerializeField] protected float maxMove = 3f;
 
     protected override void Start()
     {
@@ -34,14 +36,14 @@ public class BGLGreen : LightBlink
         originY = tmp.y;
         currentAngle = startAngle;
         #endregion
-
-
+        
         StartCoroutine(StartTimeSeting());
         
     }
 
     private void SelectC()
     {
+        
         if (coroutineNum == 0)
             StartCoroutine(LightCircleStar());
         else if (coroutineNum == 1)
@@ -49,17 +51,24 @@ public class BGLGreen : LightBlink
         else if (coroutineNum == 2)
             StartCoroutine(LightCircle());
     }
+    
+
+
 
     //현재 높이는 8;
 
     IEnumerator StartTimeSeting()
     {
         yield return new WaitForSeconds(FRandom());
+        currentAngle = FRandom(currentAngle);
+        divAngle = FRandom(20f, 61f);
         SelectC();
         yield return null;
 
     }
 
+    
+    #region NomalMode
     #region LightCircleStar
     IEnumerator LightCircleStar()
     {
@@ -161,11 +170,20 @@ public class BGLGreen : LightBlink
         }
     }
     #endregion
+    #endregion
 
-
+   
     float FRandom()
     {
-        return UnityEngine.Random.Range(0.0f, 2.1f);
+        return UnityEngine.Random.Range(0.0f, 1.1f);
+    }
+    float FRandom(float num)
+    {
+        return UnityEngine.Random.Range(num, num+100f);
+    }
+    float FRandom(float num1, float num2)
+    {
+        return UnityEngine.Random.Range(num1, num2);
     }
     float IRandom()
     {
